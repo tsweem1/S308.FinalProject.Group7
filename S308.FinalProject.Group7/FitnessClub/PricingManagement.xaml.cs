2de83c969684e3f0681a8adc180cca65fd06f14b
tsweem1 <tsweem@indiana.edu>
2018-03-29 15:10:44 -0400

    Adding pricing management window

--- S308.FinalProject.Group7/FitnessClub/PricingManagement.xaml.cs
+++ S308.FinalProject.Group7/FitnessClub/PricingManagement.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Pricing_Management
+namespace FitnessClub
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PricingManagement.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PricingManagement : Window
     {
-        public MainWindow()
+        public PricingManagement()
         {
             InitializeComponent();
         }
