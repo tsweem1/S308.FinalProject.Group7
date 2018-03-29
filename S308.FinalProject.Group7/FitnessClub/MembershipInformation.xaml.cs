254e0fbc599b4ecf17c64eb7953405ef2d3e53cd
tifpham@iu.edu <tifpham@iu.edu>
2018-03-29 15:16:39 -0400

    created membership information wpf

--- S308.FinalProject.Group7/FitnessClub/MembershipInformation.xaml.cs
+++ S308.FinalProject.Group7/FitnessClub/MembershipInformation.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Membership_Information
+namespace FitnessClub
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MembershipInformation.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MembershipInformation : Window
     {
-        public MainWindow()
+        public MembershipInformation()
         {
             InitializeComponent();
         }
