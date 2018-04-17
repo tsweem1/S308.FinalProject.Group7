2335e5da24cf02f58584e5baef8de39ec9618445
Evelyn Delph <evdelph@indiana.edu>
2018-04-17 00:44:50 -0400

    started coding

---
+++ S308.FinalProject.Group7/FitnessClub/CreditCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub
+{
+    class CreditCard : Member
+    {
+        private bool isValid = false;
+        private long lngNum;
+        private string strErrorMessage;
+        public void CardNumValid()
+        {
+            if(!Int64.TryParse(CreditCardNumber,out lngNum))
+            {
+                strErrorMessage = "Credit card numbers contain only numbers.";
+                return;            
+            }
+
+        }
+
+        public void CheckCreditLength()
+        {
+            if(CreditCardNumber.Length != 13 && CreditCardNumber.Length != 15 && CreditCardNumber.Length != 16)
+            {
+                strErrorMessage = "Credit card numbers must contain 13, 15, or 16 digits";
+                return;
+            }
+
+        }
+
+    }
+}
+
---
+++ S308.FinalProject.Group7/FitnessClub/Member.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub
+{
+   //Add class for member
+    public class Member
+    {
+
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public string EmailAddress { get; set; }
+        public string CreditCardType { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string SecurityCode { get; set; }
+    
+
+        public Member()
+        {
+
+        }
+
+        //Initialize constructor
+        public Member(string userID, string firstName, string lastName, string phoneNumber, string gender, DateTime birthday, string email, string creditType, string creditNumber, string securityCode )
+        {
+            UserId = userID;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Gender = gender;
+            Birthday = birthday;
+            EmailAddress = email;
+            CreditCardType = creditType;
+            CreditCardNumber = creditNumber;
+            SecurityCode = securityCode;
+       }
+
+        // Output for data grids...probably add more for output
+        public override string ToString()
+        {
+            string strOutput = "";
+
+            strOutput += "First Name" + Environment.NewLine;
+            strOutput += "Last Name" + Environment.NewLine;
+            strOutput += "Phone" + Environment.NewLine;
+            strOutput += "Gender" + Environment.NewLine;
+            strOutput += "Birthday" + Environment.NewLine;
+            strOutput += "Email" + Environment.NewLine;
+
+            return strOutput;
+        }
+    }
+}
