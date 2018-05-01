744e93178b386ae2b202b84c69a3061e1c2d02f7
Tiffany Pham <tifpham@iu.edu>
2018-04-30 22:55:00 -0400

    Merch Quote Psuedo Code - Done

--- S308.FinalProject.Group7/FitnessClub/MerchandiseQuotation.xaml.cs
+++ S308.FinalProject.Group7/FitnessClub/MerchandiseQuotation.xaml.cs
@@ -27,21 +27,23 @@ namespace FitnessClub
         public Merchandise()
         {
             InitializeComponent();
-            // Load Json File
+            //1. Load Json File
             GetDataSetFromFile();
         }
         private void GetDataSetFromFile()
         {
+            //2. Create new list to hold merch info
             List<MerchandiseInfo> lstMember = new List<MerchandiseInfo>();
+            //3. createe string to hold file path
             string strFilePath = @"..\..\..\Data\MerchInfo.json";
 
             try
             {
-               //use system.oi.file to read the entire data file
+               //4.use system.oi.file to read the entire data file
                 StreamReader reader = new StreamReader(strFilePath);
                 string jsonData = reader.ReadToEnd();
                 reader.Close();
-                                //serialize the json data to a list of customers
+               //5. serialize the json data to a list of customers
                 merchList = JsonConvert.DeserializeObject<List<MerchandiseInfo>>(jsonData);
             }
             catch (Exception ex)
@@ -49,10 +51,10 @@ namespace FitnessClub
                 MessageBox.Show("Error loading Merchandise from file: " + ex.Message);
             }
 
-            //add combobox items
+            //6. add combobox items
             AddComboItems();
         }
-        //add merchandise to combobox
+        //7. add merchandise to combobox
         public void AddComboItems()
         {
             foreach (var i in merchList)
@@ -61,7 +63,8 @@ namespace FitnessClub
 
         }
 
-
+        #region Helper Functions
+        //8. Naviage to Main Menu Window
         private void btnMainMenu1_Click(object sender, RoutedEventArgs e)
         {
             Window1 winMainMenu = new Window1();
@@ -69,7 +72,7 @@ namespace FitnessClub
             this.Close();
         }
 
-
+        //9. Clear Contents
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
             cboMerchandise.Text = "";
@@ -78,17 +81,19 @@ namespace FitnessClub
             cboSize.Text = "";
             txtShoppingCart.Text = "";
         }
+        #endregion
 
+        
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            //declare variables
+            //10. declare variables
             int intQty;
             string strShoppingCart;
             double dblTotalPrice;
             string strSize;
 
     
-            //validate user enters in merch
+            //11. validate user enters in merch
             if (cboMerchandise.SelectedIndex == -1)
             {
                 MessageBox.Show("Please choose an item from our merchandise selection.");
@@ -99,46 +104,47 @@ namespace FitnessClub
                 MessageBox.Show("Please select a size.");
                 return;
             }
-            //validate user enters in a valid number for quantity
+            //12. validate user enters in a valid number for quantity
             if (!int.TryParse(txtQty.Text, out intQty))
             {
                
                 MessageBox.Show("Please enter a valid number for quantity.");
                 return;
             }
-            //validate user enters in a valid number for quantity and does not leave it blank
+            //13. validate user enters in a valid number for quantity and does not leave it blank
             if (intQty <0 && txtQty.Text == "")
             {
                 MessageBox.Show("Please enter a valid amount for quantity.");
                 return;
             }
 
-            //Set selected item to string for query
+            //14. Set selected item to string for query
             string strMerchSelection = cboMerchandise.SelectedItem.ToString();
 
-            //get price from drop down
+            //15. get price from drop down
             var priceQuery =
                 from p in merchList
                where (p.MerchItem.Trim() == strMerchSelection.Trim())
                 select p.MerchPrice;
 
-            //Convert query list to string
+            //16. Convert query list to string
             string strMerchPrice = String.Join(",", priceQuery);
 
-            //convert output for total price calculation and display in shopping cart
+            //17. convert output for total price calculation and display in shopping cart
             double dblMerchPrice = Convert.ToDouble(strMerchPrice);
             intQty = Convert.ToInt32(txtQty.Text);
             strSize = Convert.ToString(cboSize.Text);
 
-            //calculate total price
+            //18. calculate total price
             dblTotalPrice = dblMerchPrice * intQty;
 
-            //shopping cart
+            //19. shopping cart
            strShoppingCart = Environment.NewLine + "Item:".PadRight(15) + strMerchSelection.ToString() +Environment.NewLine +Environment.NewLine+ "Quantity:".PadRight(15) + intQty.ToString()+ Environment.NewLine+ Environment.NewLine+ "Size:".PadRight(15) + strSize.ToString() + Environment.NewLine + "_____________________________________" + Environment.NewLine + Environment.NewLine+"Total Price:".PadRight(15) + dblTotalPrice.ToString("C2");
 
-            //set shopping cart output
+            //20. set shopping cart output
             txtShoppingCart.Text = strShoppingCart;
-            // Set merch output to corresponding text boxes
+
+            //21. Set merch output to corresponding text boxes
             txtPrice.Text = dblMerchPrice.ToString("C2");
             txtQty.Text = intQty.ToString();
             cboSize.Text = strSize.ToString();
